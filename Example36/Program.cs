e0b314864daaa065be4738bcfa3967224656f16c
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-05-23 23:50:51 +0300

    Решили 36 задачу

--- Example36/Program.cs
+++ Example36/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+void NewArrayOfNumberRandom(int [] array)
+{
+Random randomNumber = new Random();
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randomNumber.Next(-999,999);
+    }
+    Console.Write("["); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i !=array.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+  Console.Write("]");
+}
+int SumElementArray(int [] array)
+{
+    int result = 0;
+    for (var i = 1; i < array.Length; i+=2)
+    {
+        result = result + array[i];
+    }
+    return result;
+}
+Console.Write("Введите размер массива: ");
+int sizeArray=Convert.ToInt32(Console.ReadLine());
+int [] RandomArray = new int [sizeArray];
+NewArrayOfNumberRandom(RandomArray);
+int findSumArray=SumElementArray(RandomArray);
+System.Console.Write($" - > {findSumArray}");
