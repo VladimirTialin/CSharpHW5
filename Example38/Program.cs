727faafaef711e0eb74dee359a68a5b000c6ba8e
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-05-29 23:22:21 +0300

    Добаработан алгоритм с вариативностью к задаче 38

--- Example38/Program.cs
+++ Example38/Program.cs
@@ -2,15 +2,31 @@
 Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76
 */
-double ArrayRealNumbers(double [] ArrayNumbers, int Factor)
+//Функция заполнения массива автоматически
+double[] ArrayRandomNumbers(double [] ArrayNumbers, int Factor)
 {
-    double Max=ArrayNumbers[0];
-    double Min=Max;
     var RandomNumbers = new Random();
     for (var i = 0; i < ArrayNumbers.Length; i++)
     {
         ArrayNumbers[i] = Math.Round(RandomNumbers.NextDouble()*Factor,2);
     }
+    return ArrayNumbers;
+}
+//Функция заполнения массива вручную
+double[] ArrayNumbers(double [] ArrayNumbers)
+{
+    Console.WriteLine($"\nВведите {ArrayNumbers.Length} любых чисел: ");
+    for (var i = 0; i < ArrayNumbers.Length; i++)
+    {
+        ArrayNumbers[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return ArrayNumbers;
+}
+//Функция разници Max и Min значения
+double ArrayRealNumber(double[] ArrayNumbers)
+{
+    double Max=ArrayNumbers[0];
+    double Min=Max;
     for (var i = 0; i < ArrayNumbers.Length; i++)
     {
         if(ArrayNumbers[i]>Max)
@@ -25,24 +41,40 @@ double ArrayRealNumbers(double [] ArrayNumbers, int Factor)
     double result=Max-Min;
     return result;
 }
-Console.Write("Задайте размер массива: ");
-int size=Convert.ToInt32(Console.ReadLine());
-Console.Write("Массив будет формироваться автоматически!\nВведите любое число для формирования диапазона значений: ");
-int Factor=Convert.ToInt32(Console.ReadLine());
-double [] ArrayNumber= new double [size];
-double result = ArrayRealNumbers(ArrayNumber,Factor);
+//Функция вывода массива
 void PrintArray(double [] array)
 {
     Console.Write("[");
-    for (var i = 0; i < ArrayNumber.Length; i++)
+    for (var i = 0; i < array.Length; i++)
     {
-        Console.Write(i);
-        if(i!=ArrayNumber.Length-1)
+        Console.Write(array[i]);
+        if(i!=array.Length-1)
         {
-            Console.Write(", ");
+            Console.Write(" | ");
         }
     }
     Console.Write("]");
 }
-PrintArray(ArrayNumber);
- Console.WriteLine(" -> "+result);
+//Ввод данных
+double [] ResultArray;
+double result;
+Console.Write("Задайте размер массива: ");
+int size=Convert.ToInt32(Console.ReadLine());
+var ArrayNumber= new double [size];
+Console.Write("\nЗаполним массив вручную? Нажмите клавишу Y/N: ");
+ConsoleKeyInfo press=Console.ReadKey();
+if (press.Key==ConsoleKey.Y)
+{
+    ResultArray = ArrayNumbers(ArrayNumber);
+    PrintArray(ResultArray);
+
+}
+else
+{
+    Console.Write("\nМассив сформируется автоматически!\nВведите любое число для формирования диапазона значений: ");
+    int Factor=Convert.ToInt32(Console.ReadLine());
+    ResultArray = ArrayRandomNumbers(ArrayNumber,Factor);
+    PrintArray(ResultArray);
+}
+result=ArrayRealNumber(ResultArray);
+Console.WriteLine(" -> "+result);
