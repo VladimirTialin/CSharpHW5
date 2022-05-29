60bacbb81084273d8fc17aa4789eb5ae955c992f
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-05-30 00:17:37 +0300

    Добаработана задача 34 - много функций не бывает

--- Example34/Program.cs
+++ Example34/Program.cs
@@ -2,35 +2,43 @@
 Задача   34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
-
-
-void addNumberOfArray (int [] array)
+//Функция ввода данных в массив через Array.Resize
+int [] AddNumberOfArray (int [] array)
 {
 try
 {
     Console.WriteLine("Введите любое трехзначное число:\nДля завершения ввода нажмите Enter");
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = Convert.ToInt32(System.Console.ReadLine());
-            if(array[i]>99 && array[i]<1000)
-            {
-                Array.Resize(ref array,array.Length + 1);
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Ошибка! Вы попытались ввести число, которое < 100 или > 1000. Повторите ввод!");
-                Console.ResetColor();
-                break;
-            }
-        }
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(System.Console.ReadLine());
+        Array.Resize(ref array,array.Length + 1);
+    }
 }
 catch
 {
-   // При возникновении исключения, когда введено значение отличное от типа данных int, уменьшаем массив на единицу и выводим его
-    Array.Resize(ref array,array.Length-1);
+// При возникновении исключения, когда введено значение отличное от типа данных int, уменьшаем массив на единицу и выводим его
+Array.Resize(ref array,array.Length-1);
+} 
+return array;
+} 
+bool Error(int[] array)
+{
+    bool Error=true;
+    for (var i = 0; i < array.Length; i++)
+    {
+       if(array[i]<99 || array[i]>1000)
+       { 
+        Error=false;
+        break;  
+       }   
+    }
+    return Error;
+}
+//Функция вывода массива
+void PrintArray(int [] array)
+{
     Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
+    for (var i = 0; i < array.Length; i++)
     {
         Console.Write(array[i]);
         if(i!=array.Length-1)
@@ -39,20 +47,37 @@ catch
         }
     }
     Console.Write("]");
-}  
-    int result=0;
-    for (int i = 0; i < array.Length; i++)
+}
+
+int EvenNumberOfArray(int[] array)
+{
+int result=0;
+for (int i = 0; i < array.Length; i++)
     {
-            if (array[i] % 2 == 0 && array[i]>0)
-            {
-                result++;
+        if (array[i] % 2 == 0 && array[i]>0)
+        {
+        result++;
         }    
     } 
     if (result!=0)
     {
-    Console.Write($"-> {result}");
+    return result;
     }
+    return 0; 
 }
-
-int [] RandomNumbers = new int [1];
-addNumberOfArray(RandomNumbers);
+int [] Numbers = new int [1];
+int result;
+int [] ResultArray=AddNumberOfArray(Numbers);
+bool ShowError = Error(ResultArray);
+if(ShowError==true)
+{
+    PrintArray(ResultArray);
+    result=EvenNumberOfArray(ResultArray);
+    Console.Write($"-> {result}");    
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Ошибка! Вы попытались ввести число, которое < 100 или > 1000. Повторите ввод!");
+    Console.ResetColor();
+}
